8018d4b636e0a006243fff8de836428485a6d499
Saujanya Nagpal <40171689+saujanyanagpal104@users.noreply.github.com>
2019-08-30 14:45:25 +0530

    Revert "adding logs for test"

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,11 +32,7 @@ const getPropsFromReq = ({ host, pathname }) => {
     .split("/")
     .filter(item => item);
 
-  console.log(pathnameSlugs, "pathnameslugs");
-
   let requestedLang = pathnameSlugs[0];
-
-  console.log(requestedLang, "requestedLang");
 
   const isLangValid = languages.includes(requestedLang);
   if (isLangValid) {
@@ -45,19 +41,11 @@ const getPropsFromReq = ({ host, pathname }) => {
     requestedLang = "en";
   }
 
-  console.log(isLangValid, "isLang Valid");
-
-  console.log(requestedLang, "after if");
-
   const uid = `${pathnameWithoutTrailingSlash(
     `${host}/${pathnameSlugs.join("/")}`
   )}`
     .replace("microbrand.", "www.")
     .replace(/\//g, ".");
-
-  console.log(uid, "uid");
-
-  console.log(langMap[requestedLang], "langmap");
 
   return {
     uid,
@@ -113,8 +101,6 @@ export default class Page extends React.Component<any, any> {
             host: req.headers.host,
             pathname
           });
-          console.log(uid, "uid in microstedata");
-          console.log(reqLang, "reqLang in microstedata");
           uid = reqUID;
           lang = reqLang;
         }
@@ -153,8 +139,6 @@ export default class Page extends React.Component<any, any> {
         default:
           uidType = "microsite";
       }
-
-      console.log(uid, uidType, lang);
 
       const micrositeData = Client(req).getByUID(uidType, uid, { lang });
       const offerData = Client(req).query(
