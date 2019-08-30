31a028040c62c990bfbc9a0c623a87b7a3ef041d
Saujanya Nagpal <40171689+saujanyanagpal104@users.noreply.github.com>
2019-08-30 14:11:44 +0530

    Revert "Handling Mobile"

--- utils/helper.ts
+++ utils/helper.ts
@@ -65,5 +65,3 @@ export const createGroupBooking = (url, data) =>
   })
     .then(response => "Successful")
     .catch(error => `Error: ${error}`);
-
-export const IS_MOBILE = () => window.outerWidth < 768;
