d45c962367df58b36d88035980b78bd33cad8dab
Alex Denver <denver.dsouza@headout.com>
2019-09-02 14:43:20 +0530

    Removed Console Log

--- components/FreeTourPopup.tsx
+++ components/FreeTourPopup.tsx
@@ -23,7 +23,6 @@ export default class FreeTourPopup extends Component<any, any> {
 
   componentDidUpdate() {
     // Hack to find the CSS Transform Scale Down Value Iteratively till Popup Fits Screen (+guttter)
-    console.log("Updated");
     if (this.props.popupState && !this.state.scaleSet && !IS_MOBILE()) {
       let parentHeight = this.FTWrapElement.clientHeight; //Parent Height is same as Screen Height
       let popupHeight = this.FTPopupElement.clientHeight;
