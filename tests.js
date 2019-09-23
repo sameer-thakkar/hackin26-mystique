027456c9eeb182446996f65db0ff0a8c228e5d88
Aakash Goel <aakash.lpin@gmail.com>
2019-09-23 16:17:37 +0530

    Fx add status checker cron (#90)
    * added cron checker
    
    * fix yml
    
    * updated crontab
    
    * updated name
    
    * updated job name

---
+++ tests.js
@@ -0,0 +1,31 @@
+const signale = require("signale");
+const fetch = require("isomorphic-unfetch");
+
+// fetch("https://mystique-tests.headout.com/run.prod.json")
+fetch("http://www.json-generator.com/api/json/get/ceaegykWcy?indent=2")
+  .then(res => res.json())
+  .then(data => {
+    const { statusCode } = data.report;
+    console.log({ statusCode });
+    const fatalErrors = statusCode["500"];
+    if (fatalErrors && fatalErrors.length) {
+      fatalErrors.map(url => signale.fatal(url));
+      signale.fatal(
+        new Error(`${fatalErrors.length} URLs have 500 status code`)
+      );
+      signale.complete({
+        prefix: "[tests]",
+        message: "Build Failed!"
+      });
+      process.exit(1);
+      return;
+    }
+
+    signale.success({
+      prefix: "[tests]",
+      message: "Build Success!"
+    });
+  })
+  .catch(e => {
+    signale.fatal("Something went wrong", e);
+  });
