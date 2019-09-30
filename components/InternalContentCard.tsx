63f45ca74eacbd826af3ab5c3f47876ed339b420
Saujanya Nagpal <40171689+saujanyanagpal104@users.noreply.github.com>
2019-09-30 18:02:25 +0530

    add fallback for content page images (#107)

--- components/InternalContentCard.tsx
+++ components/InternalContentCard.tsx
@@ -14,7 +14,10 @@ export default class InternalContentCard extends PureComponent<any, any> {
               <a key={index} href={card.card_cta.url}>
                 <div className="more-reads">
                   <div className="more-reads-image">
-                    <img src={card.image_link.url} alt="image" />
+                    <img
+                      src={card.image_link.url || card.image_source.url}
+                      alt="image"
+                    />
                   </div>
                   <div className="more-reads-text">
                     <span className="more-reads-text-heading">
