72bed34904375458c916c566f2ff05784b1937b3
Aakash Goel <aakash.lpin@gmail.com>
2019-10-10 13:18:07 +0530

    allows scremaingfrog (#126)
    * allows scremaingfrog
    
    * newline

--- pages/api/robots.ts
+++ pages/api/robots.ts
@@ -1,7 +1,8 @@
 const fullDomain = req =>
   req.headers["x-forwarded-proto"] + "://" + req.headers.host;
 
-const robotsContentForStage = () => `User-agent: *\n\nDisallow: /`;
+const robotsContentForStage = () =>
+  `User-agent: Screaming Frog SEO Spider\nDisallow:\n\nUser-agent: *\nDisallow: /`;
 
 const robotsContent = domain =>
   `User-agent: *\n\nSitemap: ${domain}/sitemap.xml`;
