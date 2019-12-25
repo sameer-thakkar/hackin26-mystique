28093f09860b0b0fb7023faace8cb1208e2004d1
Saujanya Nagpal <40171689+saujanyanagpal104@users.noreply.github.com>
2019-12-25 14:35:54 +0530

    change powered by headout logo (#240)

--- constants/index.js
+++ constants/index.js
@@ -90,7 +90,7 @@ export const BANNER_PARAMS = {
 };
 
 export const POWERED_BY_HEADOUT_LOGO =
-  "https://cdn-imgix-open.headout.com/Powered%20By%20Headout/Powered%20by.png";
+  "https://cdn-imgix-open.headout.com/Powered%20By%20Headout/powered-by-logo.svg";
 
 export const DROPDOWN_ELEMENT = {
   HAMBURGER: "HAMBURGER",
