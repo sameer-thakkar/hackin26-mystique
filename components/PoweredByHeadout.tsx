ee92dd8f0978f3ff61174c9d9f4e4c238a08ab1f
Saujanya Nagpal <40171689+saujanyanagpal104@users.noreply.github.com>
2020-01-10 14:09:01 +0530

    fix powered by headout logo (#255)
    * fix powered by headout logo
    
    * changes requested done

--- components/PoweredByHeadout.tsx
+++ components/PoweredByHeadout.tsx
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 import { POWERED_BY_HEADOUT_LOGO } from "../constants";
+import { attachQueryParam } from "../utils/helper";
 
 const PoweredByHeadout = () => (
   <Fragment>
     <div className="line"></div>
     <img
-      data-src={POWERED_BY_HEADOUT_LOGO}
+      data-src={attachQueryParam(POWERED_BY_HEADOUT_LOGO, "h=40")}
+      src={attachQueryParam(POWERED_BY_HEADOUT_LOGO, "h=40&q=10")}
       alt="Powered By Headout logo"
       className="lazyload"
     />
