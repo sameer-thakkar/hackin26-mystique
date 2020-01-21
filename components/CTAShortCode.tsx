578b0d181470af14b36804201ced434c01a02d86
Alex Denver <denver.dsouza@headout.com>
2020-01-21 10:22:22 +0530

    Add Support for Aligning CTA (#267)

--- components/CTAShortCode.tsx
+++ components/CTAShortCode.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 
 const CTAShortCode = props => {
-  const { text, link } = props;
+  const { text, link, align } = props;
   return (
     <>
-      <div className="short-code-cta-container">
+      <div className={`short-code-cta-container ${align ? align : ""}`}>
         <a href={link} target="_blank">
           <div className="short-code-cta">
             <span className="short-code-cta-text">{text}</span>
           </div>
         </a>
+        <style jsx>{`
+          .center {
+            text-align: center;
+          }
+          .right {
+            text-align: right;
+          }
+        `}</style>
       </div>
     </>
   );
