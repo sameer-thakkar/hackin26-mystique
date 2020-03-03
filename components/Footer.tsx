e455683b2dfa853b7606df0911f80c55c4592412
Mohamed Shadab <mohamedshadab78@gmail.com>
2020-03-04 00:14:23 +0530

    chore: more stuff (#337)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,13 +17,44 @@ export default class Footer extends Component<any, any> {
         <div className="line-and-disclaimer">
           <hr className="footer-line" />
           <div className="disclaimer">
-            <span className="disclaimer-text">{disclaimer}</span>
-            <Link href="/terms">
-              <span className="" style={{ float: 'left' }}>
+            <div
+              style={{ textAlign: 'left', fontSize: 16, fontFamily: 'Graphik' }}
+            >
+              {disclaimer}
+            </div>
+            <div className="terms-new">
+              <div
+                className="terms"
+                style={{
+                  float: 'left',
+                  marginBottom: 10,
+                  fontSize: 16,
+                  marginTop: 20,
+                  fontFamily: 'Graphik',
+                }}
+              >
                 © 2020 Headout. All rights reserved.
-              </span>
-              <span className="terms-tab">Terms & Conditions</span>
-            </Link>
+              </div>
+              <div style={{ display: 'flex', marginBottom: 30 }}>
+                <div className="terms-tab" style={{ marginRight: 8 }}>
+                  <Link href="/terms">
+                    <div className="terms-tab" style={{ marginRight: 8 }}>
+                      Terms & Conditions
+                    </div>
+                  </Link>
+                </div>
+                <Link href="/privacy-policy">
+                  <div className="terms-tab" style={{ marginRight: 8 }}>
+                    Privacy Policy
+                  </div>
+                </Link>
+                <Link href="/company-details">
+                  <div className="terms-tab" style={{ marginRight: 8 }}>
+                    Company Details
+                  </div>
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
