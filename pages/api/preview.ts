610c939c92aba31ff582f9e764efad9c30cb87b3
Mohamed Shadab <mohamedshadab78@gmail.com>
2020-03-09 14:57:26 +0530

    fix: prismic preview error due to cookie 🤩 (#359)

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -1,28 +1,25 @@
-import Prismic from "prismic-javascript";
-import Cookies from "cookies";
-import { apiEndpoint, linkResolver } from "../../prismic-config";
+import Prismic from 'prismic-javascript';
+import { apiEndpoint, linkResolver } from '../../prismic-config';
 
 export default function handle(req, res) {
-  const cookies = new Cookies(req, res);
-  const previewRef = cookies.get(Prismic.previewCookie);
   const token = req.query.token;
 
   Prismic.getApi(apiEndpoint, { req })
     .then(async api => {
-      const redirectUri = await api.previewSession(token, linkResolver, "/");
+      const redirectUri = await api.previewSession(token, linkResolver, '/');
       const masterRef = api.refs.find(ref => {
         return ref.isMasterRef === true;
       });
-      const ref = previewRef || masterRef.ref;
+      const ref = masterRef.ref;
 
       return {
         redirectUri,
-        ref
+        ref,
       };
     })
     .then(({ redirectUri, ref }) => {
       res.writeHead(302, {
-        Location: `${redirectUri}&ref=${ref}`
+        Location: `${redirectUri}&ref=${ref}`,
       });
       res.end();
     });
