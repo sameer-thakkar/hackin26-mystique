87cfd9937d0c1e1aef0e8c5eb87820cb521f0c03
Alex Denver <dnvr.dsz@gmail.com>
2020-06-24 16:52:19 +0530

    fix: fetch on sv for blog

--- pages/blog/[...slug].tsx
+++ pages/blog/[...slug].tsx
@@ -2,9 +2,8 @@ import { Component } from 'react';
 
 export default class BlogSlugPage extends Component {
   static async getInitialProps({ res, query }) {
-    res.writeHead(301, {
-      Location: `https://blog.headout.com/${query.slug.join('/')}`,
-    });
-    res.end();
+    await fetch(`https://blog.headout.com/${query.slug.join('/')}`)
+      .then((r) => r.json())
+      .then((r) => res.json(r));
   }
 }
