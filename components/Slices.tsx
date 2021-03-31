94ffedcec8cf717be0a16c5cfaaa48b433921f24
Hemang Shrimali <hemangshrimali22@gmail.com>
2021-03-31 16:04:30 +0530

    fix:added tours_list check (#648)
    * fx:added tours_list check
    
    * fx:added tours_list check updated
    
    * fix: toursList check

--- components/Slices.tsx
+++ components/Slices.tsx
@@ -48,6 +48,8 @@ const Listicle = dynamic(() => import('./slices/Listicle'));
 const Reviews = dynamic(() => import('./slices/Reviews'));
 
 const sliceHandler = (slice, props: any = {}) => {
+  if(slice.primary.hide_slice)
+  return null;
   switch (slice.slice_type) {
     case 'rich_text':
     case 'rich_text_only':
@@ -448,7 +450,12 @@ const sliceHandler = (slice, props: any = {}) => {
           type={slice.primary.type}
           reviews={slice.items}
         />
-      );
+      ); 
+    case 'tours_list':
+        return(
+          props.tourListSection
+        )
+    
     default:
     // ToDo: Add to Error Logs (Slice)
   }
