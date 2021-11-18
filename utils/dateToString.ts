aa4ac73b930c6678184b508312dd75697fd3e08d
aish0509 <59099860+aish0509@users.noreply.github.com>
2021-11-19 00:31:39 +0530

    fix: rating schema + showpage date format

--- utils/dateToString.ts
+++ utils/dateToString.ts
@@ -1,16 +1,23 @@
 import dayjs from 'dayjs';
 import { strings } from 'const/strings';
 import advancedFormat from 'dayjs/plugin/advancedFormat';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
 dayjs.extend(advancedFormat);
+dayjs.extend(customParseFormat);
 
 export const dateToString = (
   date,
   currentLanguage = 'en',
   dateFormat = 'DD MMM YYYY'
 ) => {
-  const today = dayjs().format('YYYY-MM-DD');
-  const tomorrow = dayjs().add(1, 'day').format('YYYY-MM-DD');
-  if (date === today) return strings.TODAY;
-  if (date === tomorrow) return strings.TOMORROW;
-  return dayjs(date).locale(currentLanguage).format(dateFormat);
+  const today = [dayjs().format('YYYY-MM-DD'), dayjs().format('DD-MM-YYYY')];
+  const tomorrow = [
+    dayjs().add(1, 'day').format('YYYY-MM-DD'),
+    dayjs().add(1, 'day').format('DD-MM-YYYY'),
+  ];
+  if (today.indexOf(date) > -1) return strings.TODAY;
+  if (tomorrow.indexOf(date) > -1) return strings.TOMORROW;
+  return dayjs(date, ['DD-MM-YYYY', 'YYYY-MM-DD'])
+    .locale(currentLanguage)
+    .format(dateFormat);
 };
