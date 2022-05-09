96db3b04b5d53b636dc46c029b46e1fa846415c3
Aishwariya <59099860+aish0509@users.noreply.github.com>
2022-05-09 15:49:34 +0530

    fix: fix condition for category tour list slice (#963)
    * fix: fix condition for category tour list slice
    
    * fix: remove initial empty array

--- components/MicrositeV2/index.tsx
+++ components/MicrositeV2/index.tsx
@@ -209,9 +209,10 @@ class MicrositeV2 extends Component<any, any> {
     };
 
     const hasCategoryTourList = categoryTourListData
-      ? Object.keys(categoryTourListData)?.length > 1
+      ? Object.keys(categoryTourListData)?.length > 1 &&
+        CMSBody?.filter((body) => body.slice_type === 'tour_list_category')
+          ?.length > 0
       : false;
-
     let tourListCategorySortBy,
       tourListCategories,
       tourListCategoryAllTours = {};
@@ -220,7 +221,7 @@ class MicrositeV2 extends Component<any, any> {
     if (hasCategoryTourList) {
       const tourListSlice = CMSBody?.filter(
         (body) => body.slice_type === 'tour_list_category'
-      )?.reduce((acc, curr) => acc + curr, []);
+      )?.reduce((acc, curr) => acc + curr);
       tourListCategorySortBy = tourListSlice?.primary?.disable_sort_selector;
       tourListCategories = tourListSlice?.items?.map((item) => {
         const {
