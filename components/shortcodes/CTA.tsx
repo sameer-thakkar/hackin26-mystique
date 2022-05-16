c8b664d842d2b3f0b039764e072793a567a0dca8
John Samuel <jhnsml.31@gmail.com>
2022-05-16 15:46:56 +0530

    feat: extend cta shortcode (#967)
    * feat: extend cta shortcode
    
    * Update components/shortcodes/CTA.tsx
    
    Co-authored-by: Amit Madnawat <amitmadnawat@gmail.com>
    
    * docs: update jsdoc
    
    Co-authored-by: Amit Madnawat <amitmadnawat@gmail.com>

--- components/shortcodes/CTA.tsx
+++ components/shortcodes/CTA.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import Conditional from 'components/common/Conditional';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { legacyBooleanCheck } from 'utils';
 
 import Button from '../UI/Button';
 
@@ -17,35 +19,72 @@ const CTAWrapper = styled.div`
 `;
 
 /**
- *
- * Use the `cta` shortcode to embed a button with a link in any Rich Text field.
- *
- * Example Use:
- *
- * ```js
- * {cta text="Read More" link="https://www.headout.com/" type="fillGradient" align="center"}
- * ```
- *
- * Properties available:
- *
- * - `text`
- *  - The text inside the button (CTA)
- * - `link`
- *  - The link the CTA points too (regular link not a hyperlink)
- * - `type`
- *  - The type of button that will appear. Can be <a href="http://headout.github.io/mystique?path=/docs/ui-button--bordered">bordered</a>, <a href="http://headout.github.io/mystique?path=/docs/ui-button--fill">fill</a>, <a href="http://headout.github.io/mystique?path=/docs/ui-button--fill-gradient">fillGradient</a>, <a href="http://headout.github.io/mystique?path=/docs/ui-button--white-bordered">whiteBordered</a> (bordered is default)
- * - `align`
- *  - The alignment of the button. Can be 'center', 'left' or 'right'
+ * CTAProps is an object with a text, link, align, fill, type, and download property. The text and link
+ * properties are required, and the rest are optional.
+ * @example - {cta text="Read More" link="https://www.headout.com/" type="fillGradient" align="center"}
+ * @property {string} text - The text that will be displayed in the button
+ * @property {string} link - The URL to link to.
+ * @property {string} align - left, right, center
+ * @property {string} fill - The background color of the button. If fill is 'true' we use 'fillGradient'.
+ * @property {string} type - The type of button. 'bordered' (default), 'whiteBordered', 'fill', 'fillGradient'.
+ * @property {string} download - If you want to download a file, you can set this to 'true'.
  */
 
-const CTA = (props) => {
-  const { text, link, align, fill, type } = props;
+type CTAProps = {
+  text: string;
+  link: string;
+  align?: string;
+  fill?: string;
+  type?: string;
+  download?: string;
+};
+
+/**
+ * It's a React component that renders a button or a link depending on the value of the `download` prop
+ * @param {CTAProps}  - `text` - The text that will be displayed on the button.
+ * @returns A component that renders a button or a link depending on the value of the download prop.
+ */
+
+const CTA = ({
+  text,
+  link: url,
+  align = 'left',
+  fill,
+  type,
+  download,
+}: CTAProps) => {
+  const [blobUrl, setBlobUrl] = useState(null);
+  const isDownload = legacyBooleanCheck(download);
+  const anchorTagProps = isDownload
+    ? {
+        download: true,
+      }
+    : {
+        target: '_blank',
+        rel: 'noopener noreferrer',
+      };
+
+  useEffect(() => {
+    async function fetchImageBlob(url) {
+      const res = await fetch(url);
+      const blob = await res.blob();
+      const blobUrl = URL.createObjectURL(blob);
+      setBlobUrl(blobUrl);
+    }
+    fetchImageBlob(url);
+  }, [url]);
+
   return (
-    <CTAWrapper {...{ align }}>
-      <a href={link} target="_blank" rel="noopener noreferrer">
-        <Button fillType={fill ? 'fillGradient' : type}>{text}</Button>
-      </a>
-    </CTAWrapper>
+    <>
+      <CTAWrapper {...{ align }}>
+        <a href={isDownload ? blobUrl : url} {...anchorTagProps}>
+          <Conditional if={isDownload}>{text}</Conditional>
+          <Conditional if={!isDownload}>
+            <Button fillType={fill ? 'fillGradient' : type}>{text}</Button>
+          </Conditional>
+        </a>
+      </CTAWrapper>
+    </>
   );
 };
 
