613006af3314d8a664878fe4fea55ba21c330a3f
charlesdenon <100593078+charlesdenon@users.noreply.github.com>
2022-11-01 09:09:19 +0530

    Ft ondemand env cluster (#1173)
    * add helm chart and ondemand pipeline
    
    * version change

---
+++ ci/scripts/on-demand-deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+
+# catch exit 1 (which is a special case in grep meaning 'no matches') so that we can use pipefail
+_grep() { grep "$@" || test $? = 1; }
+aws eks \
+ --region ${AWS_REGION} \
+   update-kubeconfig --name ${CLUSTER_NAME}
+
+aws ecr get-login-password \
+     --region ap-south-1 | helm registry login \
+     --username AWS \
+     --password-stdin 999499138329.dkr.ecr.ap-south-1.amazonaws.com
+
+# Check if namespace exists and create it if it doesn't.
+
+KUBE_NAMESPACE_EXISTS=$(kubectl get namespaces | _grep ^${DEPLOY_NAMESPACE} )
+if [ -z "${KUBE_NAMESPACE_EXISTS}" ]; then
+    echo "The environment ${DEPLOY_NAMESPACE} does not exists. Please Create Environment from k8s-deployment..."
+    exit 0
+fi
+
+helm upgrade -i mystique ${HELM_REPOSITORY}/mystique -n ${DEPLOY_NAMESPACE} --set image.repository="999499138329.dkr.ecr.us-east-1.amazonaws.com/headout/mystique" --set image.tag=${MYSTIQUE_V} \
+ --set environmentVariables.NEXT_PUBLIC_BASE_URL=https://${DEPLOY_NAMESPACE}.deimos.test-headout.com \
+ --set environmentVariables.NEXT_PUBLIC_HEADOUT_API_DOMAIN=https://${DEPLOY_NAMESPACE}.calipso.test-headout.com
