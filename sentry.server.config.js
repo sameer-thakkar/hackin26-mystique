609d576530deaac028775534598c9129f8a42fef
Vibhor Chaturvedi <114465851+vibhor-web-headout@users.noreply.github.com>
2022-11-07 13:31:32 +0530

    fix: uncomment local-host url & add sampling to client trace events (#1187)

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -10,10 +10,10 @@ const SENTRY_DSN =
 Sentry.init({
   dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0.0,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
-  // ignoreUrls: [/https?:\/\/(www\.)?(localhost)\.com/],
+  ignoreUrls: [/https?:\/\/(www\.)?(localhost)\.com/],
 });
