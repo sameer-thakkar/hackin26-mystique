f1a19b66fbc1e32fcd8e3a3ab2e7f58614d3cf4b
Gautam Arora <gautamarora6248@gmail.com>
2023-04-05 11:00:20 +0530

    chore: remove springfest banner (#1452)

--- components/MicrositeV1.tsx
+++ components/MicrositeV1.tsx
@@ -36,8 +36,6 @@ import { strings } from 'const/strings';
 import renderShortCodes from 'utils/shortCodes';
 import { getLogoRedirectionUrl, convertUidToUrl } from 'utils/urlUtils';
 
-import SpringTheaterFestBanner from './SpringTheaterFestBanner';
-
 const LongForm = dynamic(() => import('components/common/LongForm'));
 const FreeTourPopup = dynamic(() => import('./FreeTourPopup'), { ssr: false });
 const GroupBooking = dynamic(() => import('./GroupBooking'), { ssr: false });
@@ -341,7 +339,6 @@ const MicrositeV1 = (props: any) => {
   const finalHeaderLinks =
     headerLinks && !isHeaderInherited ? headerLinks : null;
   const isCollectionMicrobrand = isCollectionMB(mbType);
-  const isSpringBannerVisible = uid === 'www.london-tickets.co.uk';
 
   useEffect(() => {
     setIsMobile(windowWidth < 768);
@@ -528,9 +525,6 @@ const MicrositeV1 = (props: any) => {
             hideCTA={isToursAvailable ? hideBannerCTA : true}
             orderedTgids={orderedTgids}
           />
-          <Conditional if={isSpringBannerVisible}>
-            <SpringTheaterFestBanner />
-          </Conditional>
         </Conditional>
         <Conditional if={mbTheme !== THEMES.MIN_BLUE && isCollectionMicrobrand}>
           <StaticBanner
