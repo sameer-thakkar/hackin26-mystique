27321107299dd88dd22d50599f1c05c89516955e
srivsaks <99582144+srivsaks@users.noreply.github.com>
2023-04-11 12:55:36 +0530

    fix: fix sentry issue https://headout.sentry.io/issues/4040774137/?project=1545593&query=is%3Aunresolved&referrer=issue-stream&statsPeriod=14d&stream_index=0 because of empty host issue (#1459)

--- contexts/MBContext.tsx
+++ contexts/MBContext.tsx
@@ -123,7 +123,7 @@ export const MBContextProvider = (props: any) => {
     see_more_text: microsite?.see_more_text,
   };
   const nakedDomain = !host?.includes('localhost')
-    ? host.replace('stage-', '').split('.').slice(1).join('.')
+    ? host?.replace('stage-', '').split('.').slice(1).join('.')
     : 'headout.com';
 
   return (
