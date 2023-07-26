09dc03eb9c080169f01a57c88ef836b3904affac
Aayush Dongre <44211899+AayushDongre@users.noreply.github.com>
2023-07-26 14:38:37 +0530

    fix: browse by categories section background color  (#1600)
    * fix: icon mismatch in pinned card
    
    * fix: browse by categories background color

--- components/MicrositeV2/LttLandingPageV2/BrowseByCategoriesSection/style.tsx
+++ components/MicrositeV2/LttLandingPageV2/BrowseByCategoriesSection/style.tsx
@@ -8,7 +8,6 @@ export const CategoriesSection = styled.div`
   flex-direction: column;
   align-items: start;
   justify-content: start;
-  background-color: #f8f6ff;
   p {
     ${expandFontToken(FONTS.DISPLAY_REGULAR)};
     color: ${COLORS.GRAY.G2};
@@ -69,6 +68,7 @@ export const CategoriesSection = styled.div`
     width: 100vw;
     padding: 1.5rem 1.5rem 1.75rem;
     border-radius: 4px;
+    background-color: #f8f6ff;
 
     p {
       ${expandFontToken(FONTS.HEADING_SMALL)};
