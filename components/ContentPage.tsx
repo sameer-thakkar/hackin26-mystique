ec4d6648a976323f6aff5d4bf6f36b6c162d0b9a
Arghya Biswas <arghyabiswas2000@gmail.com>
2023-08-16 13:26:19 +0530

    fix: h2 spacing (#1622)

--- components/ContentPage.tsx
+++ components/ContentPage.tsx
@@ -144,6 +144,10 @@ const StyledContentPage = styled.div`
     padding: 0;
   }
 
+  .slice-block.rich_text p + h2 {
+    margin: 1.5rem 0 0.25rem;
+  }
+
   @media (max-width: 768px) {
     grid-row-gap: 3rem;
     .page_tabs + div {
@@ -175,6 +179,9 @@ const StyledContentPage = styled.div`
     .slice-wrapper.slice-block {
       padding: 0 16px;
       width: calc(100% - 32px);
+    }
+    .slice-block.rich_text p + h2 {
+    margin: 1.25rem 0 0.25rem;
     }
   }
 `;
