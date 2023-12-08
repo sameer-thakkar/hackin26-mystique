9488457cdf5559da34d996af126a409b848f8fe3
Vibhor Chaturvedi <114465851+vibhor-web-headout@users.noreply.github.com>
2023-12-08 10:46:13 +0530

    fix: add null check for constant-case method (#1913)

--- utils/stringUtils.ts
+++ utils/stringUtils.ts
@@ -28,12 +28,12 @@ export const titleCase = (str: string) => {
   });
 };
 
-export const constantCase = (str: string) => {
+export const constantCase = (str: string = '') => {
   return str
-    .replace(/([a-z])([A-Z])/g, '$1_$2') // convert any camelCase to snake_case
-    .replace(/(\d)/g, '_$1_') // add an underscore before and after any numbers
-    .replace(/[\s.-]+/g, '_') // replace any spaces, dots, or hyphens with underscores
-    .toUpperCase(); // convert all letters to uppercase
+    ?.replace(/([a-z])([A-Z])/g, '$1_$2') // convert any camelCase to snake_case
+    ?.replace(/(\d)/g, '_$1_') // add an underscore before and after any numbers
+    ?.replace(/[\s.-]+/g, '_') // replace any spaces, dots, or hyphens with underscores
+    ?.toUpperCase(); // convert all letters to uppercase
 };
 
 export const characterLimitStrings = (str: string, limit: number) => {
