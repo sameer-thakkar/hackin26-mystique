10ff67da53a3fbf40ba3c5eb49de58efee367336
Dhruv <51574716+Illusion47586@users.noreply.github.com>
2024-01-30 14:30:46 +0530

    fix: unused variable and formatting on codemod (#2023)

--- codemod/svg/updateImports.js
+++ codemod/svg/updateImports.js
@@ -1,6 +1,5 @@
 // update-imports.js
-const { parse, join } = require('path');
-const { writeFileSync } = require('fs');
+const { join } = require('path');
 
 module.exports = function (file, api) {
   const j = api.jscodeshift;
@@ -14,34 +13,35 @@ module.exports = function (file, api) {
   const getLastSegment = (path) => path.split('/').pop();
 
   // Update import statements
-  root.find(j.ImportDeclaration, {
-    source: {
-      value: (path) => path.includes('/assets/SvgIcons'),
-    },
-  }).forEach((path) => {
-    const sourceFilePath = file.path;
-    const { dir } = parse(sourceFilePath);
+  root
+    .find(j.ImportDeclaration, {
+      source: {
+        value: (path) => path.includes('/assets/SvgIcons'),
+      },
+    })
+    .forEach((path) => {
+      // Extract the last segment of the path (potential import name)
+      const lastSegment = getLastSegment(path.value.source.value);
+      const iconName = toPascalCase(lastSegment.replace('.tsx', ''));
 
-    // Extract the last segment of the path (potential import name)
-    const lastSegment = getLastSegment(path.value.source.value);
-    const iconName = toPascalCase(lastSegment.replace('.tsx', ''));
+      // Construct the new import path
+      const updatedImportPath = join('assets', iconName.toLowerCase());
 
-    // Construct the new import path
-    const updatedImportPath = join('assets', iconName.toLowerCase());
+      // Update the import path and specifier
+      j(path).replaceWith(
+        j.importDeclaration(
+          [j.importDefaultSpecifier(j.identifier(iconName))],
+          j.literal(updatedImportPath)
+        )
+      );
 
-    // Update the import path and specifier
-    j(path).replaceWith(
-      j.importDeclaration(
-        [j.importDefaultSpecifier(j.identifier(iconName))],
-        j.literal(updatedImportPath)
-      )
-    );
-
-    // Update usages within the file
-    root.find(j.Identifier, { name: path.node.specifiers[0].local.name }).forEach((identifierPath) => {
-      j(identifierPath).replaceWith(j.identifier(iconName));
+      // Update usages within the file
+      root
+        .find(j.Identifier, { name: path.node.specifiers[0].local.name })
+        .forEach((identifierPath) => {
+          j(identifierPath).replaceWith(j.identifier(iconName));
+        });
     });
-  });
 
   // Save the changes
   return root.toSource();
