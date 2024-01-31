5fc2a66f5c5d3a848379076a3b28b565ba6c7963
Gautam Arora <gautamarora6248@gmail.com>
2024-01-31 17:53:21 +0530

    feat: enable clarity for few domains (#2025)
    * feat: enable domains for clarity support
    
    * chore: remove www from urls

--- constants/clarityConstants.ts
+++ constants/clarityConstants.ts
@@ -6,4 +6,9 @@ export const CLARITY_SUPPORTED_DOMAINS = [
   'www.thevaticantickets.com',
   'www.colosseum-rome-tickets.com',
   'www.desert-safari-dxb.com',
+  'tickets-paris.fr',
+  'tickets-tokyo.com',
+  'new-york-tickets.com',
+  'airport-transfers.tickets-istanbul.com',
+  'airport-transfers.london-tickets.co.uk',
 ];
