c43e000d72b38ed18b688e5813b095d78cf5e4f8
Rajat Verma <rajatverma5885045@gmail.com>
2024-02-05 15:52:41 +0530

    feat: add newrelic rum monitoring (#2039)

--- components/common/NextSeoMeta.tsx
+++ components/common/NextSeoMeta.tsx
@@ -9,6 +9,7 @@ import {
   BreadcrumbsSchema,
   CollectionAggregatedRatingScript,
   MystiquePerfScript,
+  NewRelicPerformanceMonitoring,
   TrackingScripts,
   VideoMetaScript,
   WebpageJsonLD,
@@ -31,6 +32,7 @@ import { currencyListAtom } from 'store/atoms/currencyList';
 import { shortcodesAtom } from 'store/atoms/shortcodes';
 import {
   FB_DOMAIN_VERIFICATION,
+  NEWRELIC_RUM_CONFIG,
   PAGE_URL_STRUCTURE,
   QUERY_PARAMS,
   SEO_SUBDOMAINS,
@@ -315,6 +317,7 @@ export default function PopulateMeta({
           isPreview={isPreview}
           originalHost={host}
         />
+        <NewRelicPerformanceMonitoring {...NEWRELIC_RUM_CONFIG} />
       </Conditional>
       <WebpageJsonLD {...jsonLdProps} />
       <Conditional
