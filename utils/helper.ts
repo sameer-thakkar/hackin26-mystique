835a94b79082e46ad989a1aae1dbaff5d54ba30f
mdaferheadout <147055082+mdaferheadout@users.noreply.github.com>
2024-02-20 16:35:54 +0400

    fix: replace special char from title id (#2083)

--- utils/helper.ts
+++ utils/helper.ts
@@ -358,7 +358,7 @@ export const attachQueryParam = (
 };
 
 export const stringIdfy = (string: string) => {
-  return string?.trim().replace(/\s/g, '-').toLowerCase();
+  return string?.trim().replace(/[\s&]/g, '-').toLowerCase();
 };
 
 export const csvTgidToArray = (csv: string | KeyTextField | undefined) => {
