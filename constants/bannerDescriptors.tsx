2cee2465273e637c4feb0a9a8b70538b82b2f963
Aishwariya <59099860+aish0509@users.noreply.github.com>
2024-03-11 12:33:37 +0530

    chore: update day trip  descriptors (#2149)
    * chore: update day trip  descriptors
    
    * chore: roll hoho exp to 0%
    
    * chore: roll hoho exp to 0%

--- constants/bannerDescriptors.tsx
+++ constants/bannerDescriptors.tsx
@@ -90,16 +90,16 @@ export const SUB_CATEGORY_BANNER: BannerMap = () => {
     ],
     '1143': [
       {
-        icon: BANNER_DESCRIPTORS.ROUND_TRIP,
-        text: strings.BANNER_DESCRIPTORS.ROUND_TRIP,
+        icon: BANNER_DESCRIPTORS.DIAMOND,
+        text: strings.BANNER_DESCRIPTORS.HANDPICKED,
       },
       {
-        icon: BANNER_DESCRIPTORS.TRANSLATE,
-        text: strings.BANNER_DESCRIPTORS.MULTILINGUAL_GUIDES,
+        icon: BANNER_DESCRIPTORS.PLANNING,
+        text: strings.BANNER_DESCRIPTORS.EFFORTLESS,
       },
       {
-        icon: BANNER_DESCRIPTORS.PEACE,
-        text: strings.BANNER_DESCRIPTORS.CULTURAL_EXPERIENCE,
+        icon: BANNER_DESCRIPTORS.INSIGHTS,
+        text: strings.BANNER_DESCRIPTORS.EXPERT_GUIDES,
       },
     ],
     '1011': [
