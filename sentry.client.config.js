d7b274f785c680a6edf358ea5f17c087aa821af6
Rajat Verma <rajatverma5885045@gmail.com>
2024-03-18 16:20:24 +0530

    fix: upgrade browser performance tooling (#2170)

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -2,12 +2,12 @@
 // The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
+import { browserTracingIntegration, init as initSentry } from '@sentry/nextjs';
 
 const SENTRY_DSN =
   'https://a952d80706b3435388b1fb5983c74b18@o45960.ingest.sentry.io/1545593';
 
-Sentry.init({
+initSentry({
   dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 0.05,
@@ -17,4 +17,9 @@ Sentry.init({
   // that it will also get attached to your source maps
   ignoreUrls: [/https?:\/\/(www\.)?(localhost)\.com/],
   enabled: process.env.NODE_ENV !== 'development',
+  integrations: [
+    browserTracingIntegration({
+      enableInp: true,
+    }),
+  ],
 });
