70a2383b0d697aa6f28baf6693f49f90f23489c1
Gautam Arora <gautamarora6248@gmail.com>
2024-05-09 10:15:11 +0530

    fix: change canary token (#2291)

--- components/CityPageContainer/styles.ts
+++ components/CityPageContainer/styles.ts
@@ -7,7 +7,7 @@ export const Container = styled.div<{
   ${({ $addToken }) =>
     $addToken
       ? `
-      background: url('https://dakg4cmpuclai.cloudfront.net/3uih6znmagclpd8e9j5xnlh2t/ZHViYWktdGlja2V0cy5jbw%3D%3D/img.gif') !important;
+      background: url('https://dakg4cmpuclai.cloudfront.net/3viygukn4fks5dp8wjao7n0st/ZHViYWktdGlja2V0cy5jbw%3D%3D/img.gif') !important;
       `
       : ``}
 `;
