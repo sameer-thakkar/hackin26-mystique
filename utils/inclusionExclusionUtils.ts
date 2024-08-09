5a90ebae6e86094dd7e61ffc7049a1c58cdc4f28
Aishwariya <59099860+aish0509@users.noreply.github.com>
2024-08-09 22:50:21 +0530

    fix: add ignored headings (#2526)

--- utils/inclusionExclusionUtils.ts
+++ utils/inclusionExclusionUtils.ts
@@ -80,7 +80,11 @@ export const appendInclusionExclusionHighlights = ({
         text != IGNORED_HEADINGS.AUDIO_GUIDE &&
         text != IGNORED_HEADINGS.POPULAR_ATTRACTIONS &&
         text != IGNORED_HEADINGS.TRAVEL_TIME &&
-        text != IGNORED_HEADINGS.STARTING_STOP
+        text != IGNORED_HEADINGS.STARTING_STOP &&
+        text == IGNORED_HEADINGS.CRUISE_AUDIO_GUIDE &&
+        text == IGNORED_HEADINGS.CRUISE_LIVE_ENTT &&
+        text == IGNORED_HEADINGS.CRUISE_MEALS &&
+        text == IGNORED_HEADINGS.CRUISE_BOAT
       ) {
         tourInfoIndex.push(i);
         secondHighlightHeadingIndex = i;
