48ac5ad528d111c155cd1218641fbfab08c392eb
Aishwariya <59099860+aish0509@users.noreply.github.com>
2024-08-11 00:34:17 +0530

    fix: heading check condition (#2527)

--- utils/inclusionExclusionUtils.ts
+++ utils/inclusionExclusionUtils.ts
@@ -81,10 +81,10 @@ export const appendInclusionExclusionHighlights = ({
         text != IGNORED_HEADINGS.POPULAR_ATTRACTIONS &&
         text != IGNORED_HEADINGS.TRAVEL_TIME &&
         text != IGNORED_HEADINGS.STARTING_STOP &&
-        text == IGNORED_HEADINGS.CRUISE_AUDIO_GUIDE &&
-        text == IGNORED_HEADINGS.CRUISE_LIVE_ENTT &&
-        text == IGNORED_HEADINGS.CRUISE_MEALS &&
-        text == IGNORED_HEADINGS.CRUISE_BOAT
+        text != IGNORED_HEADINGS.CRUISE_AUDIO_GUIDE &&
+        text != IGNORED_HEADINGS.CRUISE_LIVE_ENTT &&
+        text != IGNORED_HEADINGS.CRUISE_MEALS &&
+        text != IGNORED_HEADINGS.CRUISE_BOAT
       ) {
         tourInfoIndex.push(i);
         secondHighlightHeadingIndex = i;
