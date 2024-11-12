9baf2ee3d3ff9bf26e55b5949f862246bf8c2ab1
mdaferheadout <147055082+mdaferheadout@users.noreply.github.com>
2024-11-12 13:34:44 +0800

    chore: pause subattractions experiment for types B & C (#2691)

--- constants/experiments.ts
+++ constants/experiments.ts
@@ -113,7 +113,7 @@ export const EXPERIMENTS: Record<string, Experiment> = {
   [SUBATTRACTIONS_EXPERIMENT]: new Experiment(
     SUBATTRACTIONS_EXPERIMENT,
     [VARIANTS.CONTROL, VARIANTS.TREATMENT],
-    [50, 50]
+    [100, 0]
   ),
   [VIDEO_THUMBNAIL_IN_BANNER]: new Experiment(
     VIDEO_THUMBNAIL_IN_BANNER,
