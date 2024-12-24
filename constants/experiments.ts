3e4eabe5e283995ce982303def36179258a91222
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2024-12-24 11:38:07 +0530

    chore: bump treatment to 10% for qna exp (#2799)

--- constants/experiments.ts
+++ constants/experiments.ts
@@ -116,7 +116,7 @@ export const EXPERIMENTS: Record<string, Experiment> = {
   [QNA_EXPERIMENT]: new Experiment(
     QNA_EXPERIMENT,
     [VARIANTS.CONTROL, VARIANTS.TREATMENT],
-    [100, 0]
+    [90, 10]
   ),
   [RANKING_EXPERIMENT_V3_REVENUE]: new Experiment(
     RANKING_EXPERIMENT_V3_REVENUE,
