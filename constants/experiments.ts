7106f9007ffd795ade30defe2ae7456731b5900a
Priyanshu Nayan <priyanshunayan456@gmail.com>
2025-02-28 15:17:37 +0530

    chore: bump to 50% (#2963)

--- constants/experiments.ts
+++ constants/experiments.ts
@@ -112,7 +112,7 @@ export const EXPERIMENTS: Record<string, Experiment> = {
   [POI_FILTERS_EXPERIMENT]: new Experiment(
     POI_FILTERS_EXPERIMENT,
     [VARIANTS.CONTROL, VARIANTS.TREATMENT],
-    [90, 10]
+    [50, 50]
   ),
   [PRODUCT_CARD_VIDEO]: new Experiment(
     PRODUCT_CARD_VIDEO,
