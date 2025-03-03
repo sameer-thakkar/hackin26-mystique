9867f89664bfdc185a4c8063601601131853c68c
Hitesh Gupta <hiteshgupta9193@users.noreply.github.com>
2025-03-03 19:16:23 +0530

    fix: day trips exp bump to 50 50 (#2969)

--- constants/experiments.ts
+++ constants/experiments.ts
@@ -76,7 +76,7 @@ export const EXPERIMENTS: Record<string, Experiment> = {
   [DAY_TRIPS_COLLECTION_MWEB]: new Experiment(
     DAY_TRIPS_COLLECTION_MWEB,
     [VARIANTS.CONTROL, VARIANTS.TREATMENT],
-    [80, 20]
+    [50, 50]
   ),
   [DAY_TRIPS_LISTICLE]: new Experiment(
     DAY_TRIPS_LISTICLE,
