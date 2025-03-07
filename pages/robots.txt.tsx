0d1ac87370ece9b72f23587df46b9c715d749c7d
John Samuel <john.samuel@headout.com>
2025-03-07 10:23:36 +0530

    fix: remove blacklist domains from robots (#2979)

--- pages/robots.txt.tsx
+++ pages/robots.txt.tsx
@@ -15,23 +15,6 @@ const robotsContent = (domain: any) => {
   return rules.join('\n');
 };
 
-const tempRobotsContent = `User-agent: *\nDisallow: /`;
-
-const blackListNoIndex = [
-  'rome-ticket.com',
-  'madrid-ticket.com',
-  'barcelonatickets.co',
-  'paris-tickets.co',
-  'versailles-tickets.com',
-  'tickets-florence.com',
-  'sevilletickets.com',
-  'milan-tickets.co',
-  'venice-tickets.co',
-  'naples-tickets.co',
-  'tickets-london.co.uk',
-  'entradas-valencia.com',
-];
-
 const indexDomains = [
   'parkguell.barcelonatickets.co',
   'camp-nou.barcelonatickets.co',
@@ -44,11 +27,6 @@ export default class RobotsTxt extends Component {
   static async getInitialProps({ res, req }: any) {
     const domain = fullDomain(req);
     let content = robotsContent(domain);
-    blackListNoIndex.forEach((item) => {
-      if (domain.includes(item)) {
-        content = tempRobotsContent;
-      }
-    });
     indexDomains.forEach((item) => {
       if (domain.includes(item)) {
         content = robotsContent(domain);
