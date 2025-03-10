9e3b882fb90fdb3e195bec9a7395b4e7092ef3e5
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2025-03-10 14:35:35 +0530

    chore: qna rollback to 100C 0T (#2982)

--- constants/experiments.ts
+++ constants/experiments.ts
@@ -122,7 +122,7 @@ export const EXPERIMENTS: Record<string, Experiment> = {
   [QNA_EXPERIMENT]: new Experiment(
     QNA_EXPERIMENT,
     [VARIANTS.CONTROL, VARIANTS.TREATMENT],
-    [80, 20]
+    [100, 0]
   ),
   [SEATMAP_EXPERIMENT]: new Experiment(
     SEATMAP_EXPERIMENT,
