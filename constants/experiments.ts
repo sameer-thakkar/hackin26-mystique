b73a2f584405fe5fff6d918525ee2362e9a37de6
Hitesh Gupta <hiteshgupta9193@users.noreply.github.com>
2025-04-22 09:46:56 +0530

    chore: day trips experiment recert to 100 control (#3077)

--- constants/experiments.ts
+++ constants/experiments.ts
@@ -71,7 +71,7 @@ export const EXPERIMENTS: Record<string, Experiment> = {
   [DAY_TRIPS_COLLECTION_DWEB]: new Experiment(
     DAY_TRIPS_COLLECTION_DWEB,
     [VARIANTS.CONTROL, VARIANTS.TREATMENT],
-    [50, 50]
+    [100, 0]
   ),
   [DAY_TRIPS_COLLECTION_MWEB]: new Experiment(
     DAY_TRIPS_COLLECTION_MWEB,
