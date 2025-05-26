ec467d4e4a05bfaca585a71e6cd12e685dab6dca
Priyanshu Nayan <priyanshunayan456@gmail.com>
2025-05-26 12:48:56 +0530

    chore: bump experiment (#3139)

--- components/MicrositeV2/ShowPageV2/hooks/useIsLTTShowPageExperiementEnabled.ts
+++ components/MicrositeV2/ShowPageV2/hooks/useIsLTTShowPageExperiementEnabled.ts
@@ -2,6 +2,10 @@ import useABTesting from 'hooks/useABTesting';
 import { checkIfLTTMB, isMobile } from 'utils/helper';
 import { VARIANTS } from 'const/experiments';
 
+const EXCLUDED_UIDS = [
+  'www.london-theater-tickets.com.vogue-inventing-the-runway-tickets',
+];
+
 export const useIsLTTShowPageExperiementEnabled = (
   uid: string,
   shouldTrackEvent = false
@@ -12,7 +16,8 @@ export const useIsLTTShowPageExperiementEnabled = (
     isEligible: isLttShowPageExperimentV2Eligible,
   } = useABTesting({
     experimentId: 'LTT_SHOW_PAGE_EXPERIMENT_V2',
-    customEligibilityCheckFn: () => checkIfLTTMB(uid) && !isMobile(),
+    customEligibilityCheckFn: () =>
+      checkIfLTTMB(uid) && !isMobile() && !EXCLUDED_UIDS.includes(uid),
     noTrack: !shouldTrackEvent,
   });
 
