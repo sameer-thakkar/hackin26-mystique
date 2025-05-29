8f82b5332c52abf94fc526f78a191780bf81de87
Dhruv <51574716+Illusion47586@users.noreply.github.com>
2025-05-29 18:11:55 +0530

    chore: scale down pinned reviews (#3146)

--- constants/experiments.ts
+++ constants/experiments.ts
@@ -125,7 +125,7 @@ export const EXPERIMENTS: Record<string, Experiment> = {
   [PINNED_REVIEWS_EXPERIMENT]: new Experiment(
     PINNED_REVIEWS_EXPERIMENT,
     [VARIANTS.CONTROL, VARIANTS.TREATMENT],
-    [50, 50]
+    [100, 0]
   ),
   [POI_FILTERS_EXPERIMENT]: new Experiment(
     POI_FILTERS_EXPERIMENT,
