9eef6ba8358b0adc2bdda88b12adb8e06a1a075b
Pranav Agarwal <p2012agarwal@gmail.com>
2025-07-08 18:22:24 +0530

    feat: rollback drops banner (#3208)

--- constants/experiments.ts
+++ constants/experiments.ts
@@ -77,7 +77,7 @@ export const EXPERIMENTS: Record<string, Experiment> = {
   [DROPS_EXPERIMENT]: new Experiment(
     DROPS_EXPERIMENT,
     [VARIANTS.CONTROL, VARIANTS.TREATMENT],
-    [50, 50]
+    [100, 0]
   ),
   [FLEXIBLE_CANCELLATION_EXPERIMENT]: new Experiment(
     FLEXIBLE_CANCELLATION_EXPERIMENT,
