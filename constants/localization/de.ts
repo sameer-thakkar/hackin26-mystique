5b0e848ec9395ff423308856efe8657bcb90e153
subash <59300356+subashstanz@users.noreply.github.com>
2025-07-21 12:07:55 +0530

    fix: fix typo in cruise section (#3215)
    Co-authored-by: atul-headout <atul.sharma@headout.com>

--- constants/localization/de.ts
+++ constants/localization/de.ts
@@ -1297,7 +1297,7 @@ _Alle oben in Großbuchstaben geschriebenen Begriffe sind in der Datenschutzrich
       COMBOS: 'Kombitickets',
     },
     MAP_OVERLAY: 'Verschieben Sie die Karte durch Klicken oder Tippen',
-    SIGHTS_COVERED: 'Wahr- zeichen',
+    SIGHTS_COVERED: 'Wahrzeichen',
     MENU: 'Speise- karte',
     BOARD_AT: 'Boarding: {0}',
     BOARDING_POINTS_AVAILABLE: '{0} mögliche Einstiegsstellen',
